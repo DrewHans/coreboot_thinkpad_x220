8a73d109eb8bd05624b0dbeec302e8c89d68fe82
DrewHans <programmer.drew.hans@gmail.com>
2022-01-29 16:18:21 -0600

    Add file detection

--- scripts/3_create_cleaned_rom.sh
+++ scripts/3_create_cleaned_rom.sh
@@ -12,6 +12,11 @@ if [ ! -f "$work_dir/$stock_bios_rom" ]; then
     exit 1
 fi
 
-$work_dir/me_cleaner/me_cleaner.py --soft-disable $work_dir/$stock_bios_rom --output $work_dir/$cleaned_bios_rom
+if [ ! -f "$work_dir/$cleaned_bios_rom" ]; then
+    $work_dir/me_cleaner/me_cleaner.py --soft-disable $work_dir/$stock_bios_rom --output $work_dir/$cleaned_bios_rom
+else
+    echo "Detected $cleaned_bios_rom at $work_dir/$cleaned_bios_rom"
+fi
+
 
 echo ""
--- scripts/5_download_vgabios.sh
+++ scripts/5_download_vgabios.sh
@@ -2,8 +2,13 @@
 
 
 work_dir=$(pwd)
+vgabin="vga-8086-0126.bin"
 
-echo "Downloading pre-extracted vgabios firmware"  # needed for booting windows
-wget https://github.com/thetarkus/x220-coreboot-guide/raw/master/vga-8086-0126.bin
+if [ ! -f "$work_dir/$vgabin" ]; then
+    echo "Downloading pre-extracted vgabios firmware"  # needed for booting windows
+    wget https://github.com/thetarkus/x220-coreboot-guide/raw/master/vga-8086-0126.bin
+else
+    echo "Detected vgabios firmware at $work_dir/$vgabin"
+fi
 
 echo ""
