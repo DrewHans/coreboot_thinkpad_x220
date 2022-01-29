da7e375597638b6bd8f60343062f49bfae9268eb
DrewHans <programmer.drew.hans@gmail.com>
2022-01-29 13:51:24 -0600

    Add bootsplash.jpg check

--- run.sh
+++ run.sh
@@ -2,10 +2,17 @@
 
 work_dir=$(pwd)
 stock_bios_rom="stock_bios.rom"
+bootsplash="bootsplash.jpg"
 
 if [ ! -f "$work_dir/$stock_bios_rom" ]; then
     echo "$work_dir/$stock_bios_rom file not found"
     echo "You need to BYOB (bring your own bios)"
+    echo "Aborting $0"
+    exit 1
+fi
+
+if [ ! -f "$work_dir/$bootsplash" ]; then
+    echo "$work_dir/$bootsplash file not found"
     echo "Aborting $0"
     exit 1
 fi
