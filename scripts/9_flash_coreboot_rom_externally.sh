1ff94dbedfd207a41af20d07278c597fdc26461d
DrewHans <programmer.drew.hans@gmail.com>
2023-03-25 14:51:56 -0500

    Add variable for 8mb chip

--- scripts/9_flash_coreboot_rom_externally.sh
+++ scripts/9_flash_coreboot_rom_externally.sh
@@ -12,10 +12,11 @@ fi
 echo "Now you are ready to flash coreboot!"
 
 programmer="ch341a_spi"
-eeprom_chip="W25Q128.V"
+eeprom_16mb_chip="W25Q128.V"
+eeprom_8mb_chip="MX25L6406E/MX25L6408E"
 
 echo "Try running:"
-echo "sudo flashrom -p $programmer -c '$eeprom_chip'"
+echo "sudo flashrom -p $programmer -c '$eeprom_16mb_chip'"
 echo ""
 
 echo "You should see that the programmer and eeprom chip are detected by flashrom"
@@ -23,7 +24,7 @@ echo "If not, then you probably need to fix your clip"
 echo ""
 
 echo "When you are ready, flash the coreboot rom by running:"
-echo "sudo flashrom -p $programmer -c '$eeprom_chip' -w $work_dir/coreboot.rom"
+echo "sudo flashrom -p $programmer -c '$eeprom_16mb_chip' -w $work_dir/coreboot.rom"
 # note to self, don't forget that passing -r instead of -w will read the chip
 
 echo ""
